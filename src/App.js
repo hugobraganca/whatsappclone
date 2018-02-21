69bfaca03e6f0ef255bf154d1e9edc36328d618e
Hugo Braganca <hugobraganca94@gmail.com>
2018-02-21 11:10:55 -0300

    Criação do component FormCadastro
     - Criação do component FormCadastro.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import FormLogin from './components/FormLogin';
+import FormCadastro from './components/FormCadastro';
 
 export default props => (
-  <FormLogin />
+  <FormCadastro />
 );
 
