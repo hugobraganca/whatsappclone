ad4e37a69324c05fe41114f739cb7e6a22fcbef4
Hugo Braganca <hugobraganca94@gmail.com>
2018-03-03 19:37:44 -0300

    conexão das variaveis de estados controladas pelo redux no formLogin, baseado no designer decorator

--- src/components/FormLogin.js
+++ src/components/FormLogin.js
@@ -1,21 +1,34 @@
 import React from 'react';
 import { View, Text, TextInput, Button, TouchableOpacity } from 'react-native';
 import { Actions } from 'react-native-router-flux';
+import { connect } from 'react-redux';
 
-export default props => (
-    <View style={{ flex: 1, padding: 10 }}>
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text style={{ fontSize: 25 }}>Whatsapp Clone</Text>
+const formLogin = props => {
+    console.log(props);
+    return (
+        <View style={{ flex: 1, padding: 10 }}>
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <Text style={{ fontSize: 25 }}>Whatsapp Clone</Text>
+            </View>
+            <View style={{ flex: 2 }}>
+                <TextInput value={props.email} style={{ fontSize: 20, height: 45 }} placeholder='E-mail' />
+                <TextInput value={props.senha} style={{ fontSize: 20, height: 45 }} placeholder='Senha' />
+                <TouchableOpacity onPress={() => Actions.formCadastro()}>
+                    <Text style={{ fontSize: 18 }} >Ainda não tem cadastro? Cadastra-se</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={{ flex: 2 }}>
+                <Button title="Acessar" color='#115E54' onPress={() => false} />
+            </View>
         </View>
-        <View style={{ flex: 2 }}>
-            <TextInput style={{ fontSize: 20, height: 45 }} placeholder='E-mail' />
-            <TextInput style={{ fontSize: 20, height: 45 }} placeholder='Senha' />
-            <TouchableOpacity onPress={() => Actions.formCadastro()}>
-                <Text style={{ fontSize: 18 }} >Ainda não tem cadastro? Cadastra-se</Text>
-            </TouchableOpacity>
-        </View>
-        <View style={{ flex: 2 }}>
-            <Button title="Acessar" color='#115E54' onPress={() => false} />
-        </View>
-    </View>
-); 
+    );
+}
+
+mapStateToProps = state => (
+    {
+        email: state.AutenticacaoReducer.email,
+        senha: state.AutenticacaoReducer.senha
+    }
+);
+
+export default connect(mapStateToProps, null)(formLogin);
---
+++ src/reducers/AutenticacaoReducer.js
@@ -0,0 +1,9 @@
+const INICIAL_STATE = {
+    nome: 'Hugo',
+    email: 'hugbraganca94@gmail.com',
+    senha: '123456'   
+}
+
+export default  (state = INICIAL_STATE, action) => {
+    return state;
+}
