e3f1d6c424143c6b0ba0dc0b39e0781b8890a7c0
Hugo Braganca <hugobraganca94@gmail.com>
2018-03-06 14:29:01 -0300

    Adição do background na tela de login e cadastro através do component 'ImageBackground'

--- src/components/FormCadastro.js
+++ src/components/FormCadastro.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import { View, TextInput, Button } from 'react-native';
+import { View, TextInput, Button, ImageBackground } from 'react-native';
 import { connect } from 'react-redux';
 import { modificaNome, modificaEmail, modificaSenha } from '../actions/AutenticacaoActions'
 
 const formCadastro = props => (
-    <View style={{ flex: 1, padding: 10 }}>
-        <View style={{ flex: 4, justifyContent: 'center' }}>
-            <TextInput value={props.nome} placeholder='Nome' style={{ fontSize: 20, height: 45 }} onChangeText={ texto => props.modificaNome(texto) } />
-            <TextInput value={props.email} placeholder='E-mail' style={{ fontSize: 20, height: 45 }} onChangeText={ texto => props.modificaEmail(texto) } />
-            <TextInput secureTextEntry value={props.senha} placeholder='Senha' style={{ fontSize: 20, height: 45 }} onChangeText={ texto => props.modificaSenha(texto) } />
+    <ImageBackground style={{ flex: 1 }} source={require('../imgs/bg.png')}>
+        <View style={{ flex: 1, padding: 10 }}>
+            <View style={{ flex: 4, justifyContent: 'center' }}>
+                <TextInput value={props.nome} placeholder='Nome' style={{ fontSize: 20, height: 45 }} onChangeText={texto => props.modificaNome(texto)} />
+                <TextInput value={props.email} placeholder='E-mail' style={{ fontSize: 20, height: 45 }} onChangeText={texto => props.modificaEmail(texto)} />
+                <TextInput secureTextEntry value={props.senha} placeholder='Senha' style={{ fontSize: 20, height: 45 }} onChangeText={texto => props.modificaSenha(texto)} />
+            </View>
+            <View style={{ flex: 1 }}>
+                <Button title='Cadastrar' color='#115E54' onPress={() => false} />
+            </View>
         </View>
-        <View style={{ flex: 1 }}>
-            <Button title='Cadastrar' color='#115E54' onPress={() => false} />
-        </View>
-    </View>
+    </ImageBackground>
 );
 
 const mapStateToProps = state => {
@@ -28,4 +30,4 @@ const mapStateToProps = state => {
     );
 }
 
-export default connect(mapStateToProps, { modificaNome, modificaEmail, modificaSenha})(formCadastro);
+export default connect(mapStateToProps, { modificaNome, modificaEmail, modificaSenha })(formCadastro);
