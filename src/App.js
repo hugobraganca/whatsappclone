80a080d3aceb84c30aa2e5fd89327bf4b0c9ee34
Hugo Braganca <hugobraganca94@gmail.com>
2018-03-06 15:06:16 -0300

    Adição da biblioteca do firebase e conexão do aplicativo ao firebase

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 
 import Routes from './Routes';
 import reducers from './reducers';
 
-export default props => (
-  <Provider store={createStore(reducers)}>
-    <Routes />
-  </Provider>
+class App extends Component {
 
-);
+  componentWillMount() {
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyD2HnYMmZH7bBuFajXImnPlChNpBY8zG1s",
+      authDomain: "whatsapp-clone-82626.firebaseapp.com",
+      databaseURL: "https://whatsapp-clone-82626.firebaseio.com",
+      projectId: "whatsapp-clone-82626",
+      storageBucket: "whatsapp-clone-82626.appspot.com",
+      messagingSenderId: "222386931042"
+    };
+    firebase.initializeApp(config);
+  }
+
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <Routes />
+      </Provider>
+    );
+  }
+
+}
+
+
+export default App;
