d880d79a639fc3be58e40dda58faff69432f17a3
Hugo Braganca <hugobraganca94@gmail.com>
2018-03-20 02:10:52 -0300

    Criação do componente Boas vindas e adição no Routes.js

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Router, Scene } from 'react-native-router-flux';
 import FormCadastro from './components/FormCadastro';
 import FormLogin from './components/FormLogin';
+import BoasVindas from './components/BoasVindas';
 
 
 export default props => (
@@ -9,6 +10,7 @@ export default props => (
     <Scene key='root'>
       <Scene key='formLogin' component={FormLogin} title='Login' />
       <Scene key='formCadastro' component={FormCadastro} title='Cadastro' />
+      <Scene key='boasvindas' component={BoasVindas} title='BemVindo' initial />
     </Scene>
   </Router>
 );
