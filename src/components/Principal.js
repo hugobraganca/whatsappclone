e812953a811cac2ae3d77851c0d1f55f5fbd47d2
Hugo Braganca <hugobraganca94@gmail.com>
2018-04-13 02:40:48 -0300

    Adiçãodo exemplo inicial do react-native-tab-view na página principal

--- src/components/Principal.js
+++ src/components/Principal.js
@@ -1,10 +1,48 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from 'react';
+import { View, StyleSheet, Dimensions } from 'react-native';
+import { TabViewAnimated, TabBar, SceneMap } from 'react-native-tab-view';
 
-const Principal = props => (
-    <View style={{ marginTop: 100 }}>
-        <Text style={{ fontSize: 18 }}>Página principal da aplicação</Text>
-    </View>
-);
+const initialLayout = {
+  height: 0,
+  width: Dimensions.get('window').width,
+};
 
-export default Principal;
+const FirstRoute = () => <View style={[ styles.container, { backgroundColor: '#ff4081' } ]} />;
+const SecondRoute = () => <View style={[ styles.container, { backgroundColor: '#673ab7' } ]} />;
+
+export default class Principal extends Component {
+  state = {
+    index: 0,
+    routes: [
+      { key: 'first', title: 'First' },
+      { key: 'second', title: 'Second' },
+    ],
+  };
+
+  _handleIndexChange = index => this.setState({ index });
+
+  _renderHeader = props => <TabBar {...props} />;
+
+  _renderScene = SceneMap({
+    first: FirstRoute,
+    second: SecondRoute,
+  });
+
+  render() {
+    return (
+      <TabViewAnimated
+        navigationState={this.state}
+        renderScene={this._renderScene}
+        renderHeader={this._renderHeader}
+        onIndexChange={this._handleIndexChange}
+        initialLayout={initialLayout}
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
