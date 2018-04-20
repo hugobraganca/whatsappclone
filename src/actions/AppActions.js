8e1e64eb02342f003afd7a2afb90041f10bea43e
Hugo Braganca <hugobraganca94@gmail.com>
2018-04-20 03:01:02 -0300

    Adição do contato no firabase

--- src/actions/AppActions.js
+++ src/actions/AppActions.js
@@ -20,7 +20,15 @@ export const adicionaContato = email => {
             .once('value')
             .then(snapshot => {
                 if (snapshot.val()) {
-                    console.log('Usuário existe');
+                    console.log(email);
+                    const { currentUser } = firebase.auth();
+                    let emailUsuarioB64 = b64.encode(currentUser.email);
+
+                    firebase.database().ref(`/usuario_contatos/${emailUsuarioB64}`)
+                        .push({ email: email, nome: 'Nome do contato' })
+                        .then(() => console.log('sucesso'))
+                        .catch(erro => console.log(erro))
+                    console.log(firebase.auth().currentUser.email);
                 } else {
                     dispatch({type: ADICIONA_CONTATO_ERRO, payload: 'E-mail informado não corresponde a um usuário válido!' })
                 }
