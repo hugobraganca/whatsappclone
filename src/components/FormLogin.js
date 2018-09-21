0ad9f19319813149d79dd9210a8dbdefeaa2162d
Hugo Bragança <hugobraganca94@gmail.com>
2018-09-21 09:41:11 -0300

    Adição da biblioteca react-native-router-flux e navegação entre a tela de login e cadastro implementada e funcionando

--- src/components/FormLogin.js
+++ src/components/FormLogin.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { View, Text, TextInput, Button } from 'react-native';
-
+import { View, Text, TextInput, Button, TouchableHighlight } from 'react-native';
+import { Actions } from 'react-native-router-flux';
 export default props => (
     <View style={{ flex: 1, padding: 10 }}>
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
@@ -9,7 +9,9 @@ export default props => (
         <View style={{ flex: 2 }}>
             <TextInput style={{ fontSize: 20, height: 45 }} placeholder='E-mail' />
             <TextInput style={{ fontSize: 20, height: 45 }} placeholder='Senha' />
-            <Text style={{ fontSize: 20 }}>Ainda não tem cadastro?  cadastra-se</Text>
+            <TouchableHighlight onPress={() => Actions.formCadastro() }>
+                <Text style={{ fontSize: 20 }}>Ainda não tem cadastro?  cadastra-se</Text>
+            </TouchableHighlight>
         </View>
         <View style={{ flex: 2 }}>
             <Button title="Acessar" color='#115E54' onPress={() => false} />
