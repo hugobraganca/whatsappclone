83b29c834a4745f112b56d7a447d8a7e2abf7ff5
Hugo Bragança <hugobraganca94@gmail.com>
2018-10-02 11:49:59 -0300

    Alterações no visual do componente TabBarMenu

--- src/components/TabBarMenu.js
+++ src/components/TabBarMenu.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StatusBar } from 'react-native';
 import { TabBar } from 'react-native-tab-view';
 
 export default props => (
-    <View>
-        <Text>Whatsapp Clone</Text>
+    <View style={{ backgroundColor: '#115E54', elevation: 4, marginBottom: 6 }}>
+        <StatusBar backgroundColor="#114D44" />
+        <View style={{ height: 50, justifyContent: 'center' }}>
+            <Text style={{fontSize: 20, color: '#fff', marginLeft: 20 }}>Whatsapp Clone</Text>
+        </View>
 
-        <TabBar {...props} />
+        <TabBar {...props} style={{ backgroundColor: '#115E54', elevation: 0 }}/>
     </View>
 );
