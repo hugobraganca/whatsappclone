f2152590ecc4fba16b344bdf7a480929a83007f5
Hugo Bragança <hugobraganca94@gmail.com>
2018-10-05 13:40:56 -0300

    Mapeando estador do redux e encaminhando via props para o componente AdicionarContato

--- src/components/AdicionarContato.js
+++ src/components/AdicionarContato.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, TextInput, Button } from 'react-native';
+import { connect } from 'react-redux';
 
-export default props => (
-    <View style={{ flex: 1, justifyContent: 'center', padding: 20  }}>
-        <View style={{ flex: 1, justifyContent: 'center'  }}>
-            <TextInput placeholder="E-mail"
-            style={{ fontSize: 20, height: 45  }}
-            onChange={ () => false }
-            />
-        </View>
-        <View style={{ flex: 1 }}>
-            <Button title="Adicionar" color="#115E54" onPress={ () => false } />
-        </View>
-    </View>
-);
+class adicionarContato extends Component {
+
+    render() {
+        return(
+            <View style={{ flex: 1, justifyContent: 'center', padding: 20  }}>
+                <View style={{ flex: 1, justifyContent: 'center'  }}>
+                    <TextInput 
+                    value={this.props.adiciona_contato_email} 
+                    placeholder="E-mail"
+                    style={{ fontSize: 20, height: 45  }}
+                    onChange={ () => false }
+                    />
+                </View>
+                <View style={{ flex: 1 }}>
+                    <Button title="Adicionar" color="#115E54" onPress={ () => false } />
+                </View>
+            </View>
+        );
+    }
+}
+
+const mapStateToProps = state => (
+    {
+        adiciona_contato_email: state.AppReducer.adiciona_contato_email
+    }
+)
+
+export default connect(mapStateToProps, null)(adicionarContato);
