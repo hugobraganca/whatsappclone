7112807f03fc2895236a2da98024bd20ba97c7ba
Hugo Bragança <hugobraganca94@gmail.com>
2018-10-08 14:50:10 -0300

    comparando o email digitado com os usuários cadastrados no firebase

--- src/actions/AppActions.js
+++ src/actions/AppActions.js
@@ -1,3 +1,5 @@
+import b64 from 'base-64';
+import firebase from 'firebase';
 import { 
     MODIFICA_ADICIONA_CONTATO_EMAIL
    } from './types';
@@ -13,6 +15,17 @@ export const modificaAdicionaContatoEmail = (email) => {
 
 export const adicionaContato = (email) => {
     console.log(email);
+    let emailB64 = b64.encode(email);
+
+    firebase.database().ref(`/contatos/${emailB64}`)
+        .once('value')
+        .then(snapshot => {
+            if(snapshot.val()) {
+                console.log('Usuário existe');
+            } else {
+                console.log("Usuário não existe");
+            }
+        })
     return {
         type : 'teste'
     }
