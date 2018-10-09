ecfe2efb00a184098bbe1cd38108ab645fb397be
Hugo Bragança <hugobraganca94@gmail.com>
2018-10-09 11:50:00 -0300

    Tratamento de usuários não cadastrados

--- src/actions/AppActions.js
+++ src/actions/AppActions.js
@@ -1,7 +1,7 @@
 import b64 from 'base-64';
 import firebase from 'firebase';
 import { 
-    MODIFICA_ADICIONA_CONTATO_EMAIL
+    MODIFICA_ADICIONA_CONTATO_EMAIL, ADICIONA_CONTATO_ERRO
    } from './types';
 
 
@@ -14,19 +14,23 @@ export const modificaAdicionaContatoEmail = (email) => {
 }
 
 export const adicionaContato = (email) => {
-    console.log(email);
-    let emailB64 = b64.encode(email);
 
-    firebase.database().ref(`/contatos/${emailB64}`)
-        .once('value')
-        .then(snapshot => {
-            if(snapshot.val()) {
-                console.log('Usuário existe');
-            } else {
-                console.log("Usuário não existe");
-            }
-        })
-    return {
-        type : 'teste'
-    }
+    return dispatch => {
+        let emailB64 = b64.encode(email);
+
+        firebase.database().ref(`/contatos/${emailB64}`)
+            .once('value')
+            .then(snapshot => {
+                if(snapshot.val()) {
+                    console.log('Usuário existe');
+                } else {
+                    dispatch(
+                        { 
+                            type: ADICIONA_CONTATO_ERRO, payload: 'E-mail informado não corresponde a um usuário válido!'
+                        }
+                        )
+                }
+            })
+        }
+
 }
