fc483aa70dd32ec786e761c7a8675d0bd8affc8b
Hugo Bragança <hugobraganca94@gmail.com>
2019-01-07 17:22:04 -0300

    armazenando as mensagens no firebase

--- src/actions/AppActions.js
+++ src/actions/AppActions.js
@@ -97,10 +97,25 @@ export const modificaMensagem = texto => {
 }
 
 export const enviarMensagem = (mensagem, contatoNome, contatoEmail) => {
-    console.log(mensagem);
-    console.log(contatoNome);
-    console.log(contatoEmail);
-    return ({
-        type: 'xyz'
-    })
+
+    // dados contato(contatoNome e contatoEmail)
+
+    //dados usuário (email)
+    const { currentUser } = firebase.auth();
+    const usuarioEmail = currentUser.email;
+
+
+    return dispatch => {
+
+        const usuarioEmailB64 = b64.encode(usuarioEmail);
+        const contatoEmailB64 = b64.encode(contatoEmail);
+
+        firebase.database().ref(`/mensagens/${usuarioEmailB64}/${contatoEmailB64}`)
+            .push({ mensagem: mensagem, tipo: 'e' })
+            .then(() => {
+                firebase.database().ref(`/mensagens/${contatoEmailB64}/${usuarioEmailB64}`)
+                .push( { mensagem, tipo: 'r' })
+                .then(() => dispatch ({type: 'xyz'}))
+            })    
+    }
 }
