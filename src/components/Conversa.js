04f9ff01f1e7dd579176f3390620295d0a3be28a
Hugo Bragança <hugobraganca94@gmail.com>
2019-01-09 17:21:10 -0300

    trocando mensagens - exibindo mensagens

--- src/components/Conversa.js
+++ src/components/Conversa.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import _ from 'lodash';
-import { View, Text, TextInput, Image, TouchableHighlight } from 'react-native';
+import { View, Text, TextInput, Image, TouchableHighlight, ListView } from 'react-native';
 import { modificaMensagem, enviarMensagem, conversaUsuaruietch } from '../actions/AppActions';
 
 class Conversa extends Component {
 
     componentWillMount() {
         this.props.conversaUsuaruietch(this.props.contatoEmail);
+        this.criaFonteDeDados(this.props.conversa);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.criaFonteDeDados(nextProps.conversa);
+    }
+
+    criaFonteDeDados(conversa) {
+        const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+        this.dataSource = ds.cloneWithRows(conversa);
     }
 
     _enviarMensagem() {
@@ -16,10 +26,25 @@ class Conversa extends Component {
         this.props.enviarMensagem(mensagem, contatoNome, contatoEmail);
     }
 
+    renderRow(texto) {
+        return (
+            <View>
+                <Text>{texto.mensagem}</Text>
+                <Text>{texto.tipo}</Text>
+            </View>
+        )
+    }
+
     render() {
         return (
             <View style={{ flex: 1, backgroundColor: '#EEE4DC', padding: 10 }}>
-                <View style={{ flex: 1, paddingBottom: 20,  }}></View>
+                <View style={{ flex: 1, paddingBottom: 20,  }}>
+                    <ListView 
+                        enableEmptySections
+                        dataSource={this.dataSource}
+                        renderRow={this.renderRow}
+                    />
+                </View>
                 <View style={{ flexDirection: 'row', height: 60 }}>
                     <TextInput
                     value={this.props.mensagem}
@@ -42,7 +67,7 @@ mapStateToProps = state => {
     });
 
     console.log(conversa);
-    
+
     return ({
         conversa,
         mensagem: state.AppReducer.mensagem
