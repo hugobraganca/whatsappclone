de32f0bc28331e3560135710de7385f42859d120
Hugo Bragança <hugobraganca94@gmail.com>
2019-01-11 16:08:12 -0300

    istando conversas - Exibindo as conversas

--- src/components/Conversas.js
+++ src/components/Conversas.js
@@ -1,25 +1,46 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ListView, TouchableHighlight } from 'react-native';
 import _ from 'lodash';
 import { connect } from 'react-redux';
 import { conversasUsuarioFetch } from '../actions/AppActions';
+import { Actions } from 'react-native-router-flux';
 
 class Conversas extends Component {
 
     componentWillMount() {
         this.props.conversasUsuarioFetch();
-        console.log('componentWillMount', this.props.conversas)
+        this.criaFonteDeDados(this.props.conversas);
     }
 
     componentWillReceiveProps(nextProps) {
-        console.log('componentWillReceiveProps: ', nextProps.conversas)
+        this.criaFonteDeDados(nextProps.conversas);
+    }
+
+    criaFonteDeDados(conversas) {
+        const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+        this.dataSource = ds.cloneWithRows(conversas);
+    }
+
+    renderRow(conversa) {
+        return (
+                <TouchableHighlight
+                    onPress={() => false}
+                    underlayColor='#fff'
+                >
+                    <View style={{ flex: 1, padding: 20, borderBottomWidth: 1, borderBottomColor: "#CCC" }}>
+                        <Text style={{ fontSize: 18,  }}>{conversa.nome}</Text>
+                    </View>
+                </TouchableHighlight>
+        )
     }
 
     render() {
         return (
-            <View>
-                <Text>Conversas</Text>
-            </View>
+            <ListView 
+                enableEmptySections
+                dataSource={this.dataSource}
+                renderRow={this.renderRow}
+            />
         );
     }
 }
